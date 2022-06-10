09f81b1680abef7215fbc7f4d82b18bb318f0b04
Malay Agarwal <malay.agarwal261016@outlook.com>
2022-06-10 21:41:20 +0530

    Implement main.py

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import pandas as pd
+from src.config import Config
+from src.ensemble import Ensemble
+
+
 def main():
-    pass
+    train_df = pd.read_csv(Config.filepath("train_prepared_both_le.csv"))
+    test_df = pd.read_csv(Config.filepath("test_prepared_both_le.csv"))
+
+    exclude = None
+
+    ensemble = Ensemble(train_df=train_df, test_df=test_df, exclude=exclude)
+
+    ensemble.train_level_one_models()
+
+    model_name = "lr"
+    result = ensemble.train_level_two_model(model_name=model_name)
+
+    submission = result[["PassengerId", "Transported"]]
+    submission.to_csv(Config.filepath("submission.csv"), index=False)
 
 
 if __name__ == "__main__":
