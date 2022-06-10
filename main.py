2a96f4e15c9a1ae49e68430b8070f91b96eeaa70
Malay Agarwal <malay.agarwal261016@outlook.com>
2022-06-10 22:12:42 +0530

    Add CLI interface

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
+import argparse
+
 import pandas as pd
 from src.config import Config
 from src.ensemble import Ensemble
 
 
-def main():
+def main(args: argparse.Namespace):
     train_df = pd.read_csv(Config.filepath("train_prepared_both_le.csv"))
     test_df = pd.read_csv(Config.filepath("test_prepared_both_le.csv"))
 
-    exclude = None
+    if (exclude := args.exclude) is not None:
+        exclude = exclude.split(",")
 
     ensemble = Ensemble(train_df=train_df, test_df=test_df, exclude=exclude)
 
     ensemble.train_level_one_models()
 
-    model_name = "lr"
+    model_name = args.l2_model
     result = ensemble.train_level_two_model(model_name=model_name)
 
     submission = result[["PassengerId", "Transported"]]
@@ -21,4 +24,26 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    desc = "Train an ensemble of models for Spaceship Titanic."
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument(
+        "-e",
+        "--exclude",
+        type=str,
+        required=False,
+        default=None,
+        help="Names of models to exclude from the ensemble, comma-separated.",
+    )
+
+    parser.add_argument(
+        "-l1t",
+        "-l1_trials",
+        type=int,
+        required=False,
+        default=100,
+        help="Number of Optuna trials for Level 1 models."
+    )
+
+    args = parser.parse_args()
+
+    main(args)
